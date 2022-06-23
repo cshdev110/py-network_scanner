f55a9e91ab5cd400334e4991bcd6b8a6a46790ed
hostuser <hostuser@hostuser.hostuser>
2022-06-23 19:26:40 +1000

    Enhencing program output using scape characters

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -10,12 +10,14 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     # with this function, scapy.srp() we send packets and receive the answer
     # [0] is set as we need just the first list only.
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
-
+    #  With verbose=false the top of the information is not printed out
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    print("-----------------------------------------")
+    print("IP\t\t\tMAC Address")
+    print("-----------------------------------------")
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("--------------------------------------")
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        print("-----------------------------------------")
 
 
 scan("192.168.180.1/24")
