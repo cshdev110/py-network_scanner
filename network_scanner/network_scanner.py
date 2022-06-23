e9e0bee1c2eb3758b1dbcc42eebb7338d56b3b61
hostuser <hostuser@hostuser.hostuser>
2022-06-23 20:19:49 +1000

    Using dictionary to sort the ips and mac addresses, and use of the deprecated lib optparse to ask for args.

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -2,6 +2,13 @@
 
 # The script needs to be executed as superuser.
 import scapy.all as scapy
+from optparse import OptionParser
+
+
+def get_arguments():
+    parser = OptionParser()
+    parser.add_option("-t", "--target", dest="ip_targets", help="Range of IPs to check out")
+    return parser.parse_args()[0]
 
 
 def scan(ip):
@@ -12,12 +19,23 @@ def scan(ip):
     # [0] is set as we need just the first list only.
     #  With verbose=false the top of the information is not printed out
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    clients_list = []
+    for element in answered_list:
+        # clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        # clients_list.append(clients_dict)
+        clients_list.append({"ip": element[1].psrc, "mac": element[1].hwsrc})  # short way
+    return clients_list
+
+
+def print_result(results_list):
+    print("Network: " + options.ip_targets)
     print("-----------------------------------------")
     print("IP\t\t\tMAC Address")
     print("-----------------------------------------")
-    for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-        print("-----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
 
-scan("192.168.180.1/24")
+options = get_arguments()
+print_result(scan(options.ip_targets))
+
